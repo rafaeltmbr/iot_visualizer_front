a3837022cf5da6f16347ff3bb7abe9f08136745a
Rafhael Payão <rafaeltmbr@gmail.com>
2022-07-22 10:27:25 -0300

    fix: wrong recently read attributes order

--- src/utils/getRecentlyReadAttributes.ts
+++ src/utils/getRecentlyReadAttributes.ts
@@ -4,4 +4,9 @@ import { getRecentReadings } from "./getRecentReadings";
 export const getRecentlyReadAttributes = (attributes: IAttribute[]) =>
   attributes
     .map((e) => ({ ...e, readings: getRecentReadings(e.readings) }))
-    .sort((a, b) => (a.readings?.[0] < b.readings?.[0] ? 1 : -1));
+    .sort((a, b) =>
+      !a.readings?.[0]?.updated_at ||
+      a.readings[0].updated_at < b.readings?.[0]?.updated_at
+        ? 1
+        : -1
+    );
